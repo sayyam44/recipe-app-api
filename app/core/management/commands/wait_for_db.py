c1d08c84f8b22ef54c31d3ef0fbf84505d4d461a
TicTacToe <sayyamkundra52@gmail.com>
2024-03-16 20:13:06 -0230

    Configure docker compose

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,33 +1,51 @@
-"""
-Django command to wait for the database to be available
-"""
+# """
+# Django command to wait for the database to be available
+# """
+# import time
+
+# from psycopg2 import OperationalError as Psycopg2OpError
+
+# from django.db.utils import OperationalError
+# from django.core.management.base import BaseCommand
+# # from django.db import connection #new changes
+
+
+# class Command(BaseCommand):
+#     """Django command to wait for database."""
+
+#     def handle(self, *args, **options):
+#         """Enrytpoint for command."""
+#         self.stdout.write('Waiting for database...')
+#         db_up = False
+#         while db_up is False:
+#             try:
+#                 # from django.db import connection
+#                 # connection.ensure_connection()
+#                 # db_up = True
+#                 self.check(databases=['default'])
+#                 # self.check() #old code
+#                 # connection.ensure_connection() #new changes
+#                 db_up = True
+#             except (Psycopg2OpError, OperationalError):
+#                 self.stdout.write('Database unavailable, waiting 1 second...')
+#                 time.sleep(1)
+#             # time.sleep(10)
+#         self.stdout.write(self.style.SUCCESS('Database available'))
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
 import time
 
-from psycopg2 import OperationalError as Psycopg2OpError
-
-from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand
-# from django.db import connection #new changes
-
-
 class Command(BaseCommand):
-    """Django command to wait for database."""
-
     def handle(self, *args, **options):
-        """Enrytpoint for command."""
         self.stdout.write('Waiting for database...')
-        db_up = False
-        while db_up is False:
+        db_conn = None
+        while not db_conn:
             try:
-                from django.db import connection
-                connection.ensure_connection()
-                db_up = True
-                # self.check(databases=['default'])
-                # self.check() #old code
-                # connection.ensure_connection() #new changes
-                db_up = True
-            except (Psycopg2OpError, OperationalError):
+                db_conn = connections['default']
+            except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-            # time.sleep(10)
-        self.stdout.write(self.style.SUCCESS('Database available'))
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
